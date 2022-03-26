e79fe461cff22364b8767688a126baf6dff85263
jannatul114 <jfmithila114@gmail.com>
2022-03-26 15:57:48 +0600

    product issue solved

--- src/App.js
+++ src/App.js
@@ -18,12 +18,18 @@ function App() {
   }, [])
 
   const cartProduct = (product) => {
-    const newCart = [...carts, product];
+    if (carts.indexOf(product) !== -1) {
+      alert('the product have already choosen')
+    }
+
+    else {
+      const newCart = [...carts, product];
+      setCarts(newCart);
+    }
     // if (newCart.length > 4) {
     // alert("You can't add cart more then 4 product")
     // }
     // else {
-    setCarts(newCart);
     // }
   }
 
@@ -53,18 +59,16 @@ function App() {
       <h1 className='text-center my-3'>Ring Chooser</h1>
       <div className='container d-flex justify-content-center my-3'>
         <div className='row d-flex justify-content-between'>
-          <div className='col-md-8'>
+          <div className='col-md-8 product-container'>
             <div className='row'>
               {
-
                 products.map(product => <Shop product={product} key={product.id} cartProduct={cartProduct}></Shop>)
               }
             </div>
           </div>
-          <div className='cart-container col-md-4 d-flex justify-content-center '>
+          <div className='cart-container col-md-4 d-flex justify-content-center cart-container'>
             <div className='bg w-100 text-center p-3 rounded-3'>
               <h4 className='fw-bold'>Selected Rings: </h4>
-
               <h5>Choosed for you:</h5>
               <div className='row d-flex align-items-center bg-light my-3 m-2 p-2 border border-secondary rounded-3 gy-2'>
                 <div className='col-md-5'>
@@ -81,9 +85,7 @@ function App() {
 
               <button className='bg-none border-2 border-danger p-2 rounded-3 btn btn-outline-danger' onClick={clearCart}>Choose Again</button>
               {
-
                 carts.map(cart => <Cart cart={cart} key={cart.id} randomProduct={randomProduct} random={random}></Cart>)
-
               }
             </div>
           </div>
